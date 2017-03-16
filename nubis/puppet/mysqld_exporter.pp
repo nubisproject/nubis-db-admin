6823fc4469d30dd43cac8cc43624817974ffa150
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-03-16 11:21:49 -0400

    puppet lint++

--- nubis/puppet/mysqld_exporter.pp
+++ nubis/puppet/mysqld_exporter.pp
@@ -1,4 +1,4 @@
-$mysqld_exporter_version = "0.9.0"
+$mysqld_exporter_version = '0.9.0'
 $mysqld_exporter_url = "https://github.com/prometheus/mysqld_exporter/releases/download/v${mysqld_exporter_version}/mysqld_exporter-${mysqld_exporter_version}.linux-amd64.tar.gz"
 notice ("Grabbing mysqld_exporter ${mysqld_exporter_version}")
 staging::file { "mysqld_exporter.${mysqld_exporter_version}.tar.gz":
